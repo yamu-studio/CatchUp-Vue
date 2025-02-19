f3a273ef54110d95ed8362d2755a92d9fdbd47a8
やむぅ。です <95989535+yamu-studio@users.noreply.github.com>
2025-02-19 22:28:09 +0900

    -add 基本構成解説用のコードを追加

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>Hello!</h1>
+  <!-- データを表示 -->
+  <p>{{ name }}</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <!-- v-on -->
+  <button v-on:click="buttonClick">button</button>
+  <!-- 省略系 -->
+  <button @click="buttonClick">button</button>
+  <p>{{ count }}回押した</p>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- コンポーネント -->
+  <HelloWorld :msg="inputName"></HelloWorld>
+
+  <!-- propsとemitを使う場合
+  <AddButton :count="count" @clicked="buttonClick"></AddButton> -->
+  <!-- v-modelでやる場合 -->
+  <AddButton v-model:count="count"></AddButton>
+
+  <!-- v-bind ⇒単方向バインド -->
+  <img v-bind:src="imgSrc" alt="" />
+  <img :src="imgSrc" alt="" />
+  <p v-bind:class="{ 'font-red': flag }">あいうえお</p>
+
+  <!-- v-model ⇒双方向バインド -->
+  <input v-model="inputName" type="text" name="" id="" />
+  <!-- ※v-bindはダメ... -->
+  <!-- <input v-bind:value="inputName" type="text" name="" id="" /> -->
+
+  <!-- v-if ⇒falseで要素が消える -->
+  <p v-if="flag">trueの時(v-if)</p>
+  <!-- v-show ⇒falseで要素が見えなくなる -->
+  <p v-show="flag">trueの時(v-show)</p>
+
+  <!-- v-for -->
+  <p v-for="n in count">{{ n }}</p>
+  <p v-for="name in nameList">{{ name }}さん</p>
+
+  <!-- v-forとv-ifの組み合わせ例 -->
+  <div v-for="n in nameList">
+    <p v-if="n == '鈴木次郎'">{{ n }} さんだよ！</p>
+    <p v-else-if="n == '田中太郎'">{{ n }}じゃん！</p>
+    <p v-else>その他</p>
+  </div>
+
+  <!-- v-html -->
+  <div v-html="htmlContent"></div>
 </template>
 
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import AddButton from "./components/AddButton.vue";
+
+// 普通の変数・定数
+let onamae = "田中";
+console.log(onamae);
+
+const nameList = ["田中太郎", "鈴木次郎", "山田三郎"];
+const htmlContent = "<p>HTMLコンテンツだよ！</p>";
+const imgSrc = "画像のURL";
+
+// 入力されたデータを入れる
+const inputName = ref("yamada");
+
+const flag = ref(false);
+const count = ref(0);
+// ボタン押したら
+// ・countを+1
+// ・true⇔false変わる
+function buttonClick() {
+  count.value++;
+  flag.value = !flag.value;
+  alert(`${count.value}回押したよう`);
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+h1 {
+  color: red;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.font-red {
+  color: red;
 }
 </style>
---
+++ src/components/AddButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="buttonClick">button</button>
+  <p>{{ count }}回押した</p>
+</template>
+
+<script setup>
+// props ⇒データを受け取る(変更できない)
+// const  = defineProps({
+//   count: {
+//     type: Number,
+//     required: true,
+//   },
+// });
+
+// emit ⇒イベントを自作できる
+// const emit = defineEmits(["clicked"]);
+
+// v-modelならこれでデータを結び付ける
+const count = defineModel("count");
+
+function buttonClick() {
+  count.value++;
+  alert(`${count.value}回押したよう`);
+  // emit("clicked");
+}
+</script>
